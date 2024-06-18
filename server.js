ab88a4d3fc9b8f49da82bd4d697b098ae65b4f71
tchado495 <126132485+tchado495@users.noreply.github.com>
2024-06-18 09:57:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@
 // const serverAddress = "ws://127.0.0.1:5000";
 //const serverAddress = 'wss://simple-websocket-server-echo.glitch.me/';
 //const serverAddress = "ws://185.199.108.153:5000";
-const serverAddress = 'wss://testwebsocket.github.io/';
+//const serverAddress = 'wss://testwebsocket.github.io/';
 //const serverAddress = "ws://192.168.43.89:5000";
 
-const ws = new WebSocket(serverAddress, {
+/*const ws = new WebSocket(serverAddress, {
     headers: {
         "user-agent": "Mozilla"
     }
@@ -19,5 +19,5 @@ ws.on('open', function() {
 
 ws.on('message', function(msg) {
     console.log("Received msg from the server: " + msg);
-});
+});*/
 
